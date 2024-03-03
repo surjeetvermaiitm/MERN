11ca8dbf047a1d1f38c43a45ce6cee3c252834dd
surjeetvermaiitm <surjeetverma2017@gmail.com>
2024-03-03 20:49:08 +0530

    updated

---
+++ week-14-Nextjs/next-with-backend/app/api/user/route.ts
@@ -0,0 +1,3 @@
+export async function GET() {
+    return Response.json({ name: "Surjeet Kumar Verma", email: "surjeetverma2017@gmail.com" })
+  }
---
+++ week-14-Nextjs/next-with-backend/app/page.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+async function getUserDetails() {
+  // const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  const response = await axios.get("http://localhost:3000/api/user")
+	return response.data;
+}
+
+export default async function Home() {
+  const userData = await getUserDetails();
+
+  return (
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
+    </div>
+  );
+}
